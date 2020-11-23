01af1b08fafb737d8608f1abf9c6466313714c97
ValiantsinMaslionik <vmaslionik@gmail.com>
2020-11-23 03:38:00 +0300

    P2T6: Add mult method (otherOperations)

--- calculator.java
+++ calculator.java
@@ -10,6 +10,10 @@ public class Calculator {
 	public int div(int a, int b) {
 		return a / b;
 	}
+
+	public int mult(int a, int b) {
+		return a * b;
+	}
 	
 	public int sub(int a, int b) {
 		return a - b;
